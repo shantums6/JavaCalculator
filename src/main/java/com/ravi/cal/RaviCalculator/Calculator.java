e8c4ce43d5b078a3dfa7ad114158c974df91800c
shantums6 <shantu.ms6@gmail.com>
2022-07-23 13:37:58 +0000

    bug is there

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -25,7 +25,7 @@ public class Calculator {
 	
 	public long subFucn(long first, long second){
 		
-		return second-first;
+		return second+first;
 	}
 	
 	public long mulFucn(long first, long second){
